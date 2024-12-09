e49d60d6158f7637709f975b813f182c4621619c
AlexPT2k22 <xande6871@gmail.com>
2024-12-09 16:45:02 +0000

    Enhance Livro class with reservation prioritization for educational books. Added logic to prioritize student reservations in adicionar_Reserva method and included necessary header files for LivroEducativo and Estudante classes.

--- src/Livro.cpp
+++ src/Livro.cpp
@@ -1,4 +1,6 @@
 #include "Livro.h"
+#include "LivroEducativo.h"
+#include "Estudante.h"
 
 Livro::Livro(string t, string a, string i, string c): Titulo(t), Autor(a), isbn(i), categoria(c)
 {
@@ -35,8 +37,22 @@ void Livro::setIsbn(string i) {
     isbn = i;
 }
 
+/**
+ * @brief Adiciona uma reserva para um leitor.
+ * 
+ * Esta função adiciona um leitor à lista de reservas do livro. Se o livro for educativo
+ * e o leitor for um estudante, a reserva é adicionada no início da lista, dando prioridade
+ * aos estudantes para livros educativos. Caso contrário, a reserva é adicionada ao final
+ * da lista.
+ * 
+ * @param LT Ponteiro para o leitor que deseja fazer a reserva.
+ */
 void Livro::adicionar_Reserva(Leitor* LT){
-    reservas.push_back(LT);
+    if (dynamic_cast<LivroEducativo*>(this) && dynamic_cast<Estudante*>(LT)) {
+        reservas.insert(reservas.begin(), LT); //adiciona o leitor no inicio da lista para livros educativos
+    } else {
+        reservas.push_back(LT);
+    }
 }
 
 void Livro::remover_Reserva(Leitor* LT){
